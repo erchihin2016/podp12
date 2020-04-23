2718c040ee8e3e25de6a7feef0b32e6bf023aa9c
KonstantinErchihin <erchihin2016@yandex.ru>
2020-04-23 21:54:04 +0500

    Initial commit

---
+++ app/src/main/java/com/example/podp12/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.podp12;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Button buttonOk = (Button) findViewById(R.id.buttonOk);
+        final Button buttonClear = (Button) findViewById(R.id.buttonClear);
+        final TextView textResult = (TextView) findViewById(R.id.textViewResult);
+        final EditText editName = (EditText) findViewById(R.id.editName);
+        final EditText editEmail = (EditText) findViewById(R.id.editEmail);
+
+
+        buttonOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String res = getString(R.string.errorEmptyFields);
+                if (!((editName.getText().toString().isEmpty()) || (editEmail.getText().toString().isEmpty()))) {
+                    res = getString(R.string.resultText1) + " " + editName.getText().toString() + " " + getString(R.string.resultText2) + " " + editEmail.getText();
+                }
+                textResult.setText(res);
+            }
+        });
+
+        buttonClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editName.setText("");
+                editEmail.setText("");
+                textResult.setText("");
+                Toast.makeText(MainActivity.this,"данные удалены",Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+
+}
